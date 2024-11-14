b79348108238454e626f9ba92adc85cbd08b4a88
unknown <daffatabianoo@gmail.com>
2024-11-14 07:50:06 +0700

    add(hooks): hooks integrated

---
+++ src/app/hooks/usePost.ts
@@ -0,0 +1,29 @@
+import { useState } from 'react';
+
+export const usePost = (url: string) => {
+  const [data, setData] = useState<any>(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const postData = async (body = {}) => {
+    setLoading(true);
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API}/api/${url}`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(body),
+      });
+      const data = await res.json();
+      setData(data);
+      setLoading(false);
+      return data;
+    } catch (err: any) {
+      setError(err?.message);
+      setLoading(false);
+    }
+  };
+
+  return { postData, data, loading, error };
+};
